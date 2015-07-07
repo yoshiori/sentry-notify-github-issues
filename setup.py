dbe05974bd77281836c53d8cc5d1dd155f05cc87
Yoshiori SHOJI <yoshiori@cookpad.com>
2015-07-07 18:27:06 +0900

    init

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+tests_require = [
+    'nose',
+]
+
+install_requires = [
+    'sentry>=5.0.0',
+]
+
+setup(
+    name='sentry-notify-github-issues',
+    version='0.0.1',
+    author='Yoshiori Shoji',
+    author_email='yoshiori@gmail.com',
+    url='http://github.com/yoshiori/sentry-notify-github-issues',
+    description='A Sentry notification plugin for GitHub issues.',
+    long_description=__doc__,
+    license='MTI',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    entry_points={
+       'sentry.apps': [
+            'notify_github_issues = sentry_notify_github_issues',
+        ],
+       'sentry.plugins': [
+            'notify_github_issues = sentry_notify_github_issues.plugin:NotifyGitHubIssuesPlugin'
+        ],
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
---
+++ src/sentry_notify_github_issues/plugin.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from sentry.plugins.bases.notify import NotificationPlugin
+from sentry.utils.strings import strip
+from sentry.utils import json
+import urllib2
+
+import sentry_notify_github_issues
+
+class NotifyGitHubIssuesForm(forms.Form):
+    repo = forms.CharField(label=_('Repository Name'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. getsentry/sentry'}),
+        help_text=_('Enter your repository name, including the owner.'))
+
+    access_token = forms.CharField(label=_('access token'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. xxxxxxxxxxxxxxxxxxxxxxxx'}),
+        help_text=_('Enter your personal access token, create here https://github.com/settings/tokens/new'))
+
+    api_endpoint = forms.CharField(label=_('API endpoint'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. https://github.example.com/api/v3/'}),
+        help_text=_('if Github Enterprise, Enter api endpoint. '), required=False)
+
+    label = forms.CharField(label=_('label'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. sentry'}),
+        help_text=_('Enter label text.'), required=False)
+
+class NotifyGitHubIssuesPlugin(NotificationPlugin):
+    slug = 'notify-github-issues'
+    title = _('GitHub Issues notification')
+    author = 'Yoshiori SHOJI'
+    author_url = 'https://github.com/yoshiori'
+    version = sentry_notify_github_issues.VERSION
+    description = "Integrate GitHub issues by linking a repository to a project."
+    resource_links = [
+        ('Bug Tracker', 'https://github.com/yoshiori/sentry-notify-github-issues/issues'),
+        ('Source', 'https://github.com/yoshiori/sentry-notify-github-issues'),
+    ]
+    conf_key = slug
+    project_conf_form = NotifyGitHubIssuesForm
+
+    def notify_users(self, group, event, fail_silently=False):
+        repo = self.get_option('repo', group.project)
+        api_endpoint = self.get_option('api_endpoint', group.project) or "https://api.github.com/"
+        url = '%srepos/%s/issues' % (api_endpoint, repo,)
+
+        title = event.error()
+        body = '%s\n\n[<a href="%s">View on Sentry</a>]' % (strip(group.culprit), group.get_absolute_url())
+        labels = self.get_option('label', group.project)
+
+        data = {
+          "title": title,
+          "body": body,
+        }
+        if labels:
+            data["labels"] = [x.strip() for x in labels.split(",")]
+
+        req = urllib2.Request(url, json.dumps(data))
+        req.add_header('User-Agent', 'sentry-notify-github-issues/%s' % self.version)
+        req.add_header('Authorization', 'token %s' % self.get_option('access_token', group.project))
+        req.add_header('Content-Type', 'application/json')
+
+        return urllib2.urlopen(req)
